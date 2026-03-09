de3bb528bc78785dce4ce80c7c8a0476ea4e86d8
Md. Abdul Baset Shahid <abdul.baset@niftycoders.com>
2026-03-10 00:07:33 +0600

    basic router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home from react router</h1>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
